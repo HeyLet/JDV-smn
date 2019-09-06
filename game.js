6c3d9ffd9f1db82ec0e27b071d02e1acd38e43b0
HeyLet <43420092+HeyLet@users.noreply.github.com>
2019-09-06 08:44:00 -0300

    versão final jogo da velha

--- game.js
+++ game.js
@@ -1,15 +1,25 @@
 var imagemX = 'Imagens/x.png'; //Local da imagem X
 var imagemO = 'Imagens/Bolinha.png'; // Local da imagem Bolinha
 var comparaO = '<img src="Imagens/Bolinha.png" style=" width: 200px; height: 200px background-size: 200px">'
-var controle = null; // variavel para preenchimento dos quadrados 
+var controle = null; // variavel para preenchimento dos quadrados
 var vencedor = '';
 var fimDeJogo = false;
 var resetar
-var placarX = 0;
-var placarO = 0;
+var placarX = +(window.localStorage.getItem('scoreX')) || 0;
+var placarO = +(window.localStorage.getItem('scoreO')) || 0;
 var numerodejogadas = 0; //todos os blocos numero de jogadas;
 var disabled = false;
-var nivel = 1;
+var nivel = 2;
+var tipo; //Verifica a primeira jogada da maquina se for b2 ou a1
+var selectedValue; //Inicializa jogo com o computador como oponente
+var chaveX = 'scoreX'
+var chaveO = 'scoreO'
+var bloquearJogada = false;
+
+window.onload = () => {
+    document.getElementById('placarXis').innerHTML = placarX;
+    document.getElementById('placarO').innerHTML = placarO;
+}
 
 
 //Função de clique
@@ -38,8 +48,6 @@ function botaoXis() {
     if (controle == null) {
         controle = false;
         document.getElementById("xis").disabled = true;
-    } else {
-
     }
 }
 
@@ -47,21 +55,31 @@ function botaoBolinha() {
     if (controle == null) {
         controle = true;
         document.getElementById("bola").disabled = true;
-    } else {
-
     }
 }
 
 function jogadaJogador(espaco) {
 
-    console.log(numerodejogadas)
+    if (!bloquearJogada) {
+        if (selectedValue == undefined) {
+            alert("ESCOLHA UM MODO DE JOGO")
+        } else {
+            if (!espaco.classList.contains("clicado")) {
+                console.log("Bloqueei");
+                bloquearJogada = true;
+                jogada(espaco)
+                if (selectedValue === "maquina") {
+                    setTimeout(() => {
+                        jogadaCpu();
 
-    if (espaco.classList.contains("clicado")) {
-
-    } else {
-        jogada(espaco)
-        jogadaCpu();
+                    }, 500);
+                } else {
+                    bloquearJogada = false;
+                }
+            }
+        }
     }
+
 }
 
 //jogada do computador
@@ -79,19 +97,248 @@ function jogadaCpu() {
     var c2 = document.getElementById("c2");
     var c3 = document.getElementById("c3");
     var vet = [a1, a2, a3, b1, b2, b3, c1, c2, c3];
-    var vetVazio = vet.filter(function(elemento) {
-        return elemento.innerHTML === "";
-    })
-    var index = Math.floor(Math.random() * vetVazio.length);
-
     setTimeout(() => {
-        jogada(vetVazio[index]);
-    }, 500);
+        bloquearJogada = false;
+        if (nivel == 1) {
+            var vetVazio = vet.filter(function(elemento) {
+                return elemento.innerHTML === "";
+            })
+            var index = Math.floor(Math.random() * vetVazio.length);
 
+        } else {
+            if (numerodejogadas === 1) {
+                if (b2.innerHTML === "") {
+                    jogada(b2)
+                    tipo = 5
+                    return
+                } else {
+                    jogada(a1)
+                    tipo = 1
+                    return
+                }
+            } else {
+                var contJogada = 0;
+                var jogar;
+                if (tipo == 1) { //testa se a primeira jogada da maquina foi na casa A1
+                    //Testa Linha 2
+                    if (b1.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = b1;
+                    }
 
+                    if (b2.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = b2;
+                    }
+
+                    if (b3.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = b3;
+                    }
+
+                    if (contJogada === 2) {
+                        jogada(jogar);
+                        return
+                    } else {
+                        contJogada = 0;
+                    }
+
+                    //Testa Linha 3
+                    if (c1.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c1;
+                    }
+
+                    if (c2.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c2;
+                    }
+
+                    if (c3.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c3;
+                    }
+
+                    if (contJogada === 2) {
+                        jogada(jogar);
+                        return
+                    } else {
+                        contJogada = 0;
+                    }
+
+                    //Testa Coluna 2
+                    if (a2.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = a2;
+                    }
+
+                    if (b2.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = b2;
+                    }
+
+                    if (c2.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c2;
+                    }
+
+                    if (contJogada === 2) {
+                        jogada(jogar);
+                        return
+                    } else {
+                        contJogada = 0;
+                    }
+
+                    //Testa Coluna 3
+                    if (c1.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c1;
+                    }
+
+                    if (c2.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c2;
+                    }
+
+                    if (c3.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c3;
+                    }
+
+                    if (contJogada === 2) {
+                        jogada(jogar);
+                        return
+                    } else {
+                        contJogada = 0;
+                        jogada(jogar);
+                        return
+                    }
+                } else if (tipo == 5) { //testa se a primeira jogada da maquina foi na casa B2
+                    //Testa Linha 1
+                    if (a1.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = a1;
+                    }
+
+                    if (a2.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = a2;
+                    }
+
+                    if (a3.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = a3;
+                    }
+
+                    if (contJogada === 2) {
+                        jogada(jogar);
+                        return
+                    } else {
+                        contJogada = 0;
+                    }
+
+                    //Testa Linha 3
+                    if (c1.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c1;
+                    }
+
+                    if (c2.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c2;
+                    }
+
+                    if (c3.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c3;
+                    }
+
+                    if (contJogada === 2) {
+                        jogada(jogar);
+                        return
+                    } else {
+                        contJogada = 0;
+                    }
+
+                    //Testa Coluna 1
+                    if (a1.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = a1;
+                    }
+
+                    if (b1.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = b1;
+                    }
+
+                    if (c1.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c1;
+                    }
+
+                    if (contJogada === 2) {
+                        jogada(jogar);
+                        return
+                    } else {
+                        contJogada = 0;
+                    }
+
+                    //Testa Coluna 3
+                    if (c1.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c1;
+                    }
+
+                    if (c2.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c2;
+                    }
+
+                    if (c3.innerHTML != "") {
+                        contJogada++
+                    } else {
+                        jogar = c3;
+                    }
+                    if (contJogada === 2) {
+                        jogada(jogar);
+                        return
+                    } else {
+                        contJogada = 0;
+                        jogada(jogar);
+                        return
+                    }
+                }
+
+            }
+        }
+
+    }, 200);
 }
 
-async function verificaVencedor() {
+function verificaVencedor() {
     var a1 = document.getElementById("a1").innerHTML;
     var a2 = document.getElementById("a2").innerHTML;
     var a3 = document.getElementById("a3").innerHTML;
@@ -109,12 +356,14 @@ async function verificaVencedor() {
     if (((a1 == b1 && a1 == c1 && a1 != "") || (a1 == a2 && a1 == a3 && a1 != "") || (a1 == b2 && a1 == c3 && a1 != ""))) {
         if (a1 == comparaO) {
             placarO += 1;
-            placarBolinha.innerHTML = placarO;
+            document.getElementById('placarO').innerHTML = placarO;
             vencedor = 'Bolinha';
+            window.localStorage.setItem(chaveO, placarO);
         } else {
             placarX += 1;
-            placarXis.innerHTML = placarX;
+            document.getElementById('placarXis').innerHTML = placarX;
             vencedor = 'XIS';
+            window.localStorage.setItem(chaveX, placarX);
         }
         fimDeJogo = true;
 
@@ -122,12 +371,14 @@ async function verificaVencedor() {
         {
             if (b2 == comparaO) {
                 placarO += 1;
-                placarBolinha.innerHTML = placarO;
+                document.getElementById('placarO').innerHTML = placarO;
                 vencedor = 'Bolinha';
+                window.localStorage.setItem(chaveO, placarO);
             } else {
                 placarX += 1;
-                placarXis.innerHTML = placarX;
+                document.getElementById('placarXis').innerHTML = placarX;
                 vencedor = 'XIS';
+                window.localStorage.setItem(chaveX, placarX);
             }
             fimDeJogo = true;
         }
@@ -135,32 +386,35 @@ async function verificaVencedor() {
     } else if (((c1 == c2 && c1 == c3) || (c3 == a3 && c3 == b3)) && c3 != "") {
         if (c1 == comparaO) {
             placarO += 1;
-            placarBolinha.innerHTML = placarO;
+            document.getElementById('placarO').innerHTML = placarO;
             vencedor = 'Bolinha';
+            window.localStorage.setItem(chaveO, placarO);
         } else {
             placarX += 1;
-            placarXis.innerHTML = placarX;
+            document.getElementById('placarXis').innerHTML = placarX;
             vencedor = 'XIS';
+            window.localStorage.setItem(chaveX, placarX);
         }
         fimDeJogo = true;
     }
     if (vencedor == "" && numerodejogadas == 9) {
-        await sleep(50);
-        alert("DEU VELHA!!");
+        fimDeJogo = true;
+
+        setTimeout(function() {
+            alert("DEU VELHA!!");
+        }, 50)
     }
 
     if (vencedor != "") {
         fimDeJogo = true;
 
-        await sleep(50);
-
-        alert("O ganhador foi o :'" + vencedor + "'");
+        setTimeout(function() {
+            alert("O ganhador foi o :'" + vencedor + "'");
+        }, 50)
     }
+
 }
-//tempo de resposta do alert
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+
 
 //Função de resetar
 function resetar() {
@@ -192,12 +446,21 @@ function resetar() {
     document.getElementById("c3").innerHTML = "";
     verificarClicado(document.getElementById("c3"))
 
-    fimDeJogo = false;
-    controle = null;
-    vencedor = "";
     numerodejogadas = 0;
     document.getElementById("bola").disabled = false;
     document.getElementById("xis").disabled = false;
+    controle = null; // variavel para preenchimento dos quadrados
+    vencedor = '';
+    fimDeJogo = false;
+    placarX = 0;
+    placarO = 0;
+    numerodejogadas = 0; //todos os blocos numero de jogadas;
+    nivel = 2;
+    tipo; //Verifica a primeira jogada da maquina se for b2 ou a
+    bloquearJogada = false;
+    tipoDejogo();
+
+
 }
 
 //função de novo jogo
@@ -205,12 +468,24 @@ function novoJogo(espaco) {
     resetar();
     placarO = 0;
     placarX = 0;
-    placarXis.innerHTML = placarX;
-    placarBolinha.innerHTML = placarO;
+    window.localStorage.setItem(chaveO, placarO);
+    window.localStorage.setItem(chaveX, placarX);
+    document.getElementById('placarXis').innerHTML = placarX;
+    document.getElementById('placarO').innerHTML = placarO;
 }
 
 function verificarClicado(elemento) {
     if (elemento.classList.contains("clicado")) {
         elemento.classList.remove("clicado")
+    }
+}
+
+
+function tipoDejogo() {
+    if (numerodejogadas == 0) {
+        selectedValue = document.getElementById("escolhaTipo").value;
+
+    } else {
+
     }
 }
